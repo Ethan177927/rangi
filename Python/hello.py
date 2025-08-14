92179f2f5c823e40f6c3475f8b84a00c62f3451c
Wonton <177927@cloud.rangitoto.school.nz>
2025-08-14 13:04:25 +1200

    committed

--- Python/hello.py
+++ Python/hello.py
@@ -1,1 +1,18 @@
-print("Hello world!")
+
+great = []
+total = 0
+x = ''
+three = [3]
+
+
+N = input("N: ")
+for num in range(int(N)+1):
+  x += str(num)
+
+for egg in range(len(x)):
+  if int(x[egg]) in three:
+    total +=1    
+  
+print(total)
+
+
